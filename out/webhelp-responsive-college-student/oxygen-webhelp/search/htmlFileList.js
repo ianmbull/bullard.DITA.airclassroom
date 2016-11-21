d573b78d9f97e6247de43475912597b280aa4a7d
ianmbull <ianmbull@vt.edu>
2016-11-21 03:16:37 -0500

    a11

---
+++ out/webhelp-responsive-college-student/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,9 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "concept/c-introduction.html";
+fl["1"]= "concept/c-overview-of-computers.html";
+fl["2"]= "concept/c-troubleshooting.html";
+fl["3"]= "tasks/t-adobe-creative.html";
+fl["4"]= "tasks/t-emergency.html";
+fl["5"]= "tasks/t-using-projector.html";
+var doStem = false;searchLoaded = true;
