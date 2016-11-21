cea61f9664e77072c8a89b431b89d2c2938c1509
ianmbull <ianmbull@vt.edu>
2016-11-21 03:24:58 -0500

    a19

---
+++ out/webhelp-responsive-teaching/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,8 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "concept/c-improve-teaching.html";
+fl["1"]= "concept/c-introduction.html";
+fl["2"]= "concept/c-troubleshooting.html";
+fl["3"]= "tasks/t-document-scanner.html";
+fl["4"]= "tasks/t-emergency.html";
+var doStem = false;searchLoaded = true;
